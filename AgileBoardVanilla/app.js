1a2d7afa880e6366ad7271a428f3767527999211
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-09-24 13:22:31 +0300

    добавление массива в окно работы и проверки

--- AgileBoardVanilla/app.js
+++ AgileBoardVanilla/app.js
@@ -1,35 +1,57 @@
 const userInputs = document.querySelector(".inputText")
 const btnAdd = document.getElementById("btnAdd")
-const blockUl = document.getElementById("task-execute")
+const blockUlExecute = document.getElementById("task-execute")
+const blockUlWork = document.getElementById("task-work")
+const blockUlCheck = document.getElementById("task-check")
+const blockUlGoot = document.getElementById("task-goot")
 
-task = []
+tasks = ["Understanding HTML CSS layouts positioning", "Adding Forms to websites", 
+"Java Script Basics", "Advanced JS working with the DOM", "Control Structures"]
 
+
+
+const blokTask = tasks.forEach((item, i, tasks) => {
+    
+    if(item === tasks[i] && item !== tasks[3] && item !== tasks[4]){
+       const newElementTasks = document.createElement("li") 
+      newElementTasks.innerHTML= tasks[i]
+      blockUlWork.append(newElementTasks)
+      item = i++
+    } else {
+        const newElementTasks = document.createElement("li") 
+         newElementTasks.innerHTML= tasks[i]
+         blockUlCheck.append(newElementTasks)
+         item = i++
+    }
+    })
+
+    
+
+
+
+// const arrayCard = () => {
+//     const newElementTasks1 = document.createElement("li")
+//     const elArray0 = tasks[0]
+//     newElementTasks1.innerHTML = elArray0
+//     blockUlWork.append(newElementTasks1)
+//     const newElementTasks2 = document.createElement("li")
+//     const elArray1 = tasks[1]
+//     newElementTasks2.innerHTML = elArray1
+//     blockUlWork.append(newElementTasks2)
+// }
+// arrayCard()
 
 const addNewTask = () => {
     const newElementTasks = document.createElement("li")
     const newTask = userInputs.value
-    newElementTasks.innerHTML = newTask
     console.log(newTask)
    if(newTask.length === 0){
     return
    }else {
     newElementTasks.innerHTML = `<div draggable=true>${newTask}</div>`
-    blockUl.append(newElementTasks)
-    task.push(newElementTasks)
-console.log(task)
+    blockUlExecute.append(newElementTasks)
+    tasks.push(`${newTask}`)
 }
-// function dragstart_handler(ev) {
-//     // Добавить id целевого элемента в объект передачи данных
-//     ev.newElementTasks.setData("text/plain", ev.target.id);
-//   }
-//   window.addEventListener("DOMContentLoaded", () => {
-    
-//     // Добавить обработчик события `dragstart`
-//     newElementTasks.addEventListener("dragstart", dragstart_handler);
-//   });
-
-
 }
-
 
 btnAdd.addEventListener("click", addNewTask)
