e5448b0668418619ae0be60a6d8dc221113d8923
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-10-03 22:37:52 +0300

    Add files via upload

--- AgileBoardVanilla/app.js
+++ AgileBoardVanilla/app.js
@@ -7,84 +7,125 @@ const blockUlWork = document.getElementById("task-work")
 const blockUlCheck = document.getElementById("task-check")
 const blockUlGoot = document.getElementById("task-goot")
 
-tasks = ["Understanding HTML CSS layouts positioning", "Adding Forms to websites", 
+const tasks = ["Understanding HTML CSS layouts positioning", "Adding Forms to websites", 
 "Java Script Basics", "Advanced JS working with the DOM", "Control Structures"]
+console.log(tasks)
 
-const blockTask = tasks.forEach((item, i, tasks) => {
-    
-    if(item === tasks[i] && item !== tasks[3] && item !== tasks[4]){
-       const newElementTasks = document.createElement("li") 
-      newElementTasks.innerHTML= `<div draggable=true>${tasks[i]}</div>` 
-      blockUlWork.append(newElementTasks)
+tasks = [
+  {title: item, status: "EXECUTE"},
+  {title: item, status: "WORK"},
+  {title: item, status: "CHECK"},
+  {title: item, status: "GOOT"}
+]
+
+const arr1 = tasks.slice(0, 3)
+console.log(arr1)
+const arr2 = tasks.slice(3, )
+console.log(arr2)
+
+const renderBlock = () => {
+      arr1.forEach((item, i) => {
+      const li = document.createElement("li") 
+      li.innerHTML= `<div draggable=true>${arr1[i]}</div>` 
+      blockUlWork.append(li)
       item = i++
-    } else {
-        const newElementTasks = document.createElement("li") 
-         newElementTasks.innerHTML= `<div draggable=true>${tasks[i]}</div>` 
-         blockUlCheck.append(newElementTasks)
-         item = i++
-    }
+      li.addEventListener("dragstart", (ev) => {
+      console.log("dragStart"); 
+      ev.currentTarget.classList.add("dragging");
+      console.log(ev.dataTransfer)
 })
+      })}
+renderBlock()
+
+//   divBlocks.addEventListener("dragover", (ev) => 
+//   ev.preventDefault()) 
+
+// ul.addEventListener("drop", (ev) => {
+//     ev.preventDefault();
+    
+// })
+// }
+
+// renderBlock()
+
+// const addTasks = () =>{
+//   arr1.push(input.value)
+
+// }
+// const blockTask = tasks.forEach((item, i, tasks) => {
+    
+//     if(item === tasks[i] && item !== tasks[3] && item !== tasks[4]){
+//        const newElementTasks = document.createElement("li") 
+//       newElementTasks.innerHTML= `<div draggable=true>${tasks[i]}</div>` 
+//       blockUlWork.append(newElementTasks)
+//       item = i++
+//     } else {
+//         const newElementTasks = document.createElement("li") 
+//          newElementTasks.innerHTML= `<div draggable=true>${tasks[i]}</div>` 
+//          blockUlCheck.append(newElementTasks)
+//          item = i++
+//     }
+// })
 
 const addNewTask = () => {
-    const newElementTasks = document.createElement("li")
+    const li = document.createElement("li")
     const newTask = userInputs.value
-    console.log(newTask)
    if(newTask.length === 0){
     return
    }else {
-    newElementTasks.innerHTML = `<div class="tasks-item" draggable=true>${newTask}</div>`
-    blockUlExecute.append(newElementTasks)
-    tasks.push(`${newTask}`)
+    li.innerHTML = `<div class="tasks-item" draggable=true>${newTask}</div>`
+    blockUlExecute.append(li)
+    arr2.push(`${newTask}`)
+    li.addEventListener("dragstart", (ev) => {
+      console.log("dragStart"); 
+      ev.currentTarget.classList.add("dragging");
+      console.log(ev.dataTransfer)
+  })
    }
+  }
 
     
     const tasksListElementWork = document.getElementById("task-work")
     const tasksListElementCheck = document.getElementById("task-check")
     const tasksListElementGoot = document.getElementById("task-goot")
-    const taskElements = newElementTasks
+    
 
-    taskElements.addEventListener("dragstart", (ev) => {
-        console.log("dragStart"); 
-        ev.currentTarget.classList.add("dragging");
-        console.log(ev.dataTransfer)
-    })
+//     taskElements.addEventListener("dragstart", (ev) => {
+//         console.log("dragStart"); 
+//         ev.currentTarget.classList.add("dragging");
+//         console.log(ev.dataTransfer)
+//     })
 
-    taskElements.addEventListener("dragend", (ev) => {
-  ev.currentTarget.classList.remove("dragging")},
-);
+//     taskElements.addEventListener("dragend", (ev) => {
+//   ev.currentTarget.classList.remove("dragging")},
+// );
 
-tasksListElementWork.addEventListener("dragover", (ev) => 
-  ev.preventDefault()) 
+// tasksListElementWork.addEventListener("dragover", (ev) => 
+//   ev.preventDefault()) 
 
-tasksListElementWork.addEventListener("drop", (ev) => {
-    ev.preventDefault();
+// tasksListElementWork.addEventListener("drop", (ev) => {
+//     ev.preventDefault();
 
-tasksListElementWork.append(taskElements)})
+// tasksListElementWork.append(taskElements)})
 
-tasksListElementCheck.addEventListener("dragover", (ev) => 
-  ev.preventDefault()) 
+// tasksListElementCheck.addEventListener("dragover", (ev) => 
+//   ev.preventDefault()) 
 
-tasksListElementCheck.addEventListener("drop", (ev) => {
-    ev.preventDefault();
+// tasksListElementCheck.addEventListener("drop", (ev) => {
+//     ev.preventDefault();
 
-tasksListElementCheck.append(taskElements)})
+// tasksListElementCheck.append(taskElements)})
 
-tasksListElementGoot.addEventListener("dragover", (ev) => 
-  ev.preventDefault()) 
+// tasksListElementGoot.addEventListener("dragover", (ev) => 
+//   ev.preventDefault()) 
 
-tasksListElementGoot.addEventListener("drop", (ev) => {
-    ev.preventDefault();
+// tasksListElementGoot.addEventListener("drop", (ev) => {
+//     ev.preventDefault();
 
-tasksListElementGoot.append(taskElements)})
+// tasksListElementGoot.append(taskElements)})
 
     
-// } else if(tasksListElementCheck.addEventListener("dragover", (ev) => 
-// ev.preventDefault()) )
-// { tasksListElementCheck.addEventListener("drop", (ev) => {
-//   ev.preventDefault();
-//   tasksListElementCheck.append(taskElements)})
-//   }
-}
+// }
 
 
 btnAdd.addEventListener("click", addNewTask)
